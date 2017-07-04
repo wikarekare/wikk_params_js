039b872c1c769c4e0f77bf16f399c65a3770ed08
robertburrowes <r.burrowes@auckland.ac.nz>
2017-07-04 18:33:34 +1200

    added accessor functions
    Also added VERSION for my tracking.

--- wikk_params-min.js
+++ wikk_params-min.js
@@ -1,1 +1,38 @@
-var wikk_params = ( function() {var VERSION = "1.0.0";var ARGV = {}; function getURLParameters(){var sURL = window.document.URL.toString();if (sURL.indexOf("?") > 0){var arrParams = sURL.split("?");var arrURLParams = arrParams[1].split("&");var i = 0;for (i = 0; i<arrURLParams.length; i++){var sParam =  arrURLParams[i].split("=");key = unescape(sParam[0]);value = sParam[1];if(ARGV[key] == null)ARGV[key] = value;else if(typeof ARGV[key] === 'string')ARGV[key] = [ ARGV[key], value ];elseARGV[key].push(value);}}}return {getURLParameters: getURLParameters,ARGV: ARGV,VERSION: VERSION}})();
+var wikk_params = ( function() {
+var VERSION = "1.0.1";
+var argv = {}; 
+function getURLParameters()
+{
+var sURL = window.document.URL.toString();
+if (sURL.indexOf("?") > 0)
+{
+var arrParams = sURL.split("?");
+var arrURLParams = arrParams[1].split("&");
+var i = 0;
+for (i = 0; i<arrURLParams.length; i++)
+{
+var sParam =  arrURLParams[i].split("=");
+key = unescape(sParam[0]);
+if (sParam[1] != "")
+value = unescape(sParam[1]);
+else
+value = null;
+if(argv[key] == null)
+argv[key] = value;
+else if(typeof argv[key] === 'string')
+argv[key] = [ argv[key], value ];
+else
+argv[key].push(value);
+}
+}
+}
+function get_argv(key) {
+return argv[key];
+}
+function version() { return VERSION }
+return {
+getURLParameters: getURLParameters,
+get_argv: get_argv,
+version: version
+}
+})();
--- wikk_params.js
+++ wikk_params.js
@@ -1,6 +1,6 @@
 var wikk_params = ( function() {
-  var VERSION = "1.0.0";
-  var ARGV = {}; //Hash of params passed into x.html in URL. eg. x.html?a=b&c=d
+  var VERSION = "1.0.1";
+  var argv = {}; //Hash of params passed into x.html in URL. eg. x.html?a=b&c=d
 
   //Parsing parameters in an HTML file, so we can use an html file as a cgi
   //and populate the form based on args passed in.
@@ -17,22 +17,32 @@ var wikk_params = ( function() {
           {
               var sParam =  arrURLParams[i].split("=");
               key = unescape(sParam[0]);
-              value = sParam[1];
-              if(ARGV[key] == null)
-                  ARGV[key] = value;
-              else if(typeof ARGV[key] === 'string')
-                  ARGV[key] = [ ARGV[key], value ];
+              if (sParam[1] != "")
+                value = unescape(sParam[1]);
+              else
+                value = null;
+                
+              if(argv[key] == null)
+                  argv[key] = value;
+              else if(typeof argv[key] === 'string')
+                  argv[key] = [ argv[key], value ];
               else 
-                  ARGV[key].push(value);
+                  argv[key].push(value);
           }
       }
   }
+  
+  function get_argv(key) {
+    return argv[key];
+  }
+  
+  function version() { return VERSION; }
 
   //return a hash of key: function pairs, with the key being the same name as the function.
-  //Hence call with web_ajax_module.function_name()
+  //Hence call with wikk_params.function_name()
   return {
     getURLParameters: getURLParameters,
-    ARGV: ARGV,
-    VERSION: VERSION
+    get_argv: get_argv,
+    version: version
   }
 })();
