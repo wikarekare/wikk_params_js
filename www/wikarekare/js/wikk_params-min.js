c62d68bde5c8f34cd78dd02983efc50e123f389d
robertburrowes <r.burrowes@auckland.ac.nz>
2023-04-17 13:56:04 +1200

    Generated output

--- www/wikarekare/js/wikk_params-min.js
+++ www/wikarekare/js/wikk_params-min.js
@@ -1,6 +1,8 @@
 var wikk_params = ( function() {
 var VERSION = "1.0.1";
-var argv = {}; 
+var argv = {}; //Hash of params passed into x.html in URL. eg. x.html?a=b&c=d
+//Parsing parameters in an HTML file, so we can use an html file as a cgi
+//and populate the form based on args passed in.
 function getURLParameters()
 {
 var sURL = window.document.URL.toString();
@@ -17,19 +19,29 @@ if (sParam[1] != "")
 value = unescape(sParam[1]);
 else
 value = null;
-if(argv[key] == null)
+if(argv[key] == null){
 argv[key] = value;
-else if(typeof argv[key] === 'string')
+}
+else if(typeof argv[key] === 'string'){
 argv[key] = [ argv[key], value ];
-else
+}
+else {
 argv[key].push(value);
 }
 }
 }
+}
 function get_argv(key) {
+if(key in argv) {
 return argv[key];
 }
-function version() { return VERSION }
+else {
+return null;
+}
+}
+function version() { return VERSION; }
+//return a hash of key: function pairs, with the key being the same name as the function.
+//Hence call with wikk_params.function_name()
 return {
 getURLParameters: getURLParameters,
 get_argv: get_argv,
